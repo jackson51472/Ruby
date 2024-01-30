9820ed5df3dfaba636a2dc6086d50cb332b6aac1
Jackson <jacksonaparesido@gmail.com>
2024-01-30 16:36:15 -0300

    Att

--- Calculadora.rb
+++ Calculadora.rb
@@ -8,7 +8,7 @@ puts "Qual tipo de conta você deseja fazer
 opc = gets.chomp.to_i
 
 while opc > 4
-    puts "Opção invalida!!!
+    puts "Opção inválida!!!
 Digite novamente: "
     opc = gets.chomp.to_i
 end
@@ -16,41 +16,41 @@ end
 
 case
 when opc == 1
-    puts "===" * 30
-    puts "Soma"
-    puts "Digite o primeiro numero"
+    puts "===" * 30, "SOMA
+
+Digite o primeiro número"
     num_one = gets.chomp.to_i
-    puts "Digite o segundo numero"
+    puts "Digite o segundo número"
     num_two = gets.chomp.to_i
 
     puts "#{num_one} + #{num_two} = #{num_one + num_two}"
 
 when opc == 2
-    puts "===" * 30
-    puts "Subtração"
-    puts "Digite o primeiro numero"
+    puts "===" * 30,"SUBTRAÇÃO
+
+Digite o primeiro número"
     num_one = gets.chomp.to_i
-    puts "Digite o segundo numero"
+    puts "Digite o segundo número"
     num_two = gets.chomp.to_i
 
     puts "#{num_one} - #{num_two} = #{num_one - num_two}"
 
 when opc == 3
-    puts "===" * 30
-    puts "Multiplicação"
-    puts "Digite o primeiro numero"
+    puts "===" * 30, "MULTIPLICAÇÃO
+
+Digite o primeiro número"
     num_one = gets.chomp.to_i
-    puts "Digite o segundo numero"
+    puts "Digite o segundo número"
     num_two = gets.chomp.to_i
 
     puts "#{num_one} X #{num_two} = #{num_one * num_two}"
 
 when opc == 4
-    puts "===" * 30
-    puts "Divisão"
-    puts "Digite o primeiro numero"
+    puts "===" * 30, "DIVISÃO
+
+Digite o primeiro número"
     num_one = gets.chomp.to_f
-    puts "Digite o segundo numero"
+    puts "Digite o segundo número"
     num_two = gets.chomp.to_f
 
     puts "#{num_one} / #{num_two} = #{num_one / num_two}"
